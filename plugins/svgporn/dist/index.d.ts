d96cccc84eaaea1eae4c9bd34b071f59b2d9138d
Joakim Uddholm <tethik@gmail.com>
2023-02-03 11:03:51 +0100

    more accidentally committed dist folders removed

--- plugins/svgporn/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Plugin, PluginRegistrator } from "@gram/core/dist/plugin";
-export default class SVGPornPlugin implements Plugin {
-    bootstrap(reg: PluginRegistrator): Promise<void>;
-}
-//# sourceMappingURL=index.d.ts.map
