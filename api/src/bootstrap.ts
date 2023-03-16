43e5946dd5c0dbc956d05dedada547fb891bdd0f
Joakim Uddholm <joakim.uddholm@klarna.com>
2023-03-16 14:36:55 +0100

    fix wrong package version (needed to be * to correctly reference the workspace packages)

--- api/src/bootstrap.ts
+++ api/src/bootstrap.ts
@@ -2,8 +2,8 @@ import { Application } from "express";
 import { DataAccessLayer } from "@gram/core/dist/data/dal";
 import { PluginCompiler } from "@gram/core/dist/plugin";
 
-import AWSPlugin from "@gram/aws";
-import SVGPornPlugin from "@gram/svgporn";
+import AWSPlugin from "@gram/aws"; // "@gram/aws": "*" in package.json
+import SVGPornPlugin from "@gram/svgporn"; // "@gram/svgpornm": "*" in package.json
 // import StaticPlugin from "@gram/plugin-static";
 // import ThreatLibPlugin from "@gram/plugin-threatlib";
 // import GithubPlugin from "@gram/plugin-github";
@@ -11,7 +11,7 @@ import SVGPornPlugin from "@gram/svgporn";
 export async function bootstrapPlugins(app: Application, dal: DataAccessLayer) {
   const compiler = new PluginCompiler(dal, app);
   await Promise.all([
-    new AWSPlugin().bootstrap(compiler),
+    new AWSPlugin().bootstrap(compiler), 
     new SVGPornPlugin().bootstrap(compiler),
     // new GithubPlugin().bootstrap(compiler),
     // new StaticPlugin().bootstrap(compiler),
