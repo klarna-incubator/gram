9acce60b3f03dc69e1666043aa75e595285001cd
Joakim Uddholm <joakim.uddholm@klarna.com>
2023-03-21 16:15:11 +0100

    pass correct variable to previousReviewer

--- plugins/static/src/notifications/review-declined.ts
+++ plugins/static/src/notifications/review-declined.ts
@@ -42,7 +42,7 @@ export const EmailReviewDeclined = () =>
         cc,
         recipients,
         ...variables,
-        previousReviewer,
+        previousReviewer: previous,
       };
     }
   );
