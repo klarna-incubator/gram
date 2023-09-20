66526863e6a3fb70e50cd0e7b2d267a67dca47d7
Joakim Uddholm <joakim.uddholm@klarna.com>
2023-09-20 18:15:27 +0200

    build: add ability to load from .env

--- config/index.ts
+++ config/index.ts
@@ -1,8 +1,10 @@
+import dotenv from "dotenv";
 import { loadConfig } from "@gram/core/dist/config";
 import { registerConfiguration } from "@gram/core/dist/config/configMap";
 import { developmentConfig } from "./development";
 
 export function initConfig() {
+  dotenv.config({ path: "../.env" });
   registerConfiguration("development", developmentConfig);
   //   registerConfiguration("staging", stagingConfig);
   //   registerConfiguration("production", productionConfig);
