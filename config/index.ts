e81f4af694017829865ba273181f22b90083d5db
Joakim Uddholm <tethik@blacknode.se>
2023-10-16 16:58:42 +0200

    chore: enable production/staging configs by default

--- config/index.ts
+++ config/index.ts
@@ -2,11 +2,13 @@ import dotenv from "dotenv";
 import { loadConfig } from "@gram/core/dist/config/index.js";
 import { registerConfiguration } from "@gram/core/dist/config/configMap.js";
 import { developmentConfig } from "./development.js";
+import { stagingConfig } from "./staging.js";
+import { productionConfig } from "./production.js";
 
 export function initConfig() {
   dotenv.config({ path: "../.env" });
   registerConfiguration("development", developmentConfig);
-  //   registerConfiguration("staging", stagingConfig);
-  //   registerConfiguration("production", productionConfig);
+  registerConfiguration("staging", stagingConfig);
+  registerConfiguration("production", productionConfig);
   loadConfig();
 }
