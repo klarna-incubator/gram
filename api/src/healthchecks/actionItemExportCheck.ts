b15fbfc2d06a9f15ee15334f4c0b62e0065e000b
Joakim Uddholm <joakim.uddholm@klarna.com>
2024-01-31 15:39:09 +0100

    SECDEV-1805 try refactoring into callback based checks

--- api/src/healthchecks/actionItemExportCheck.ts
+++ api/src/healthchecks/actionItemExportCheck.ts
@@ -5,7 +5,7 @@ import log4js from "log4js";
 const log = log4js.getLogger("notificationsCheck");
 
 export function actionItemExportCheck(dal: DataAccessLayer) {
-  return async (done: any) => {
+  return (done: any) => {
     const check: any = {
       name: "@gram/api-action-item-exports-failed",
       actionable: true,
@@ -14,21 +14,40 @@ export function actionItemExportCheck(dal: DataAccessLayer) {
       type: physical.type.INTERNAL_DEPENDENCY,
     };
 
-    try {
-      const failed = await dal.actionItemHandler.countFailures();
-      if (failed > 0) {
+    dal.actionItemHandler
+      .countFailures()
+      .then((failed) => {
+        if (failed > 0) {
+          check.healthy = false;
+          check.message = `There are ${failed} action itames that failed to export. Check the database table: action_item_failed_exports.`;
+          check.severity = physical.severity.WARNING;
+        }
+        done(physical.response(check));
+      })
+      .catch((error) => {
+        log.error(error);
         check.healthy = false;
-        check.message = `There are ${failed} action itames that failed to export. Check the database table: action_item_failed_exports.`;
+        check.message =
+          "Failed to check for failed action item exports. Please check error log for more info";
         check.severity = physical.severity.WARNING;
-      }
-    } catch (error: any) {
-      log.error(error);
-      check.healthy = false;
-      check.message =
-        "Failed to check for failed action item exports. Please check error log for more info";
-      check.severity = physical.severity.WARNING;
-    }
+        done(physical.response(check));
+      });
 
-    done(physical.response(check));
+    // try {
+    //   const failed = await
+    //   if (failed > 0) {
+    //     check.healthy = false;
+    //     check.message = `There are ${failed} action itames that failed to export. Check the database table: action_item_failed_exports.`;
+    //     check.severity = physical.severity.WARNING;
+    //   }
+    // } catch (error: any) {
+    //   log.error(error);
+    //   check.healthy = false;
+    //   check.message =
+    //     "Failed to check for failed action item exports. Please check error log for more info";
+    //   check.severity = physical.severity.WARNING;
+    // }
+
+    // done(physical.response(check));
   };
 }
