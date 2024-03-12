5c1cf7a4ccdb587c8d35d704068c2fafc7f8527a
Joakim Uddholm <joakim.uddholm@klarna.com>
2024-03-12 14:16:30 +0100

    SECDEV-1817 fix wrong slack channel in emails

--- config/notifications/EmailReviewRequestedReminder.ts
+++ config/notifications/EmailReviewRequestedReminder.ts
@@ -17,7 +17,7 @@ Please complete this review within 14 days, or the review will be automatically 
 
 You can refer to this playbook ({{playbookLink}}) on how to perform threat model reviews.
 
-If you are unable to complete this review, please decline the review in Gram or reach out to the Secure Development team at #team-ea-secure-development. 
+If you are unable to complete this review, please decline the review in Gram or reach out to the Secure Development team at #tm-secure-development-dm-kep. 
 
 Thank you.
 `.trim();
