aa101f7f1606c5364ce7fd56ffd6088d97f266bc
Joakim Uddholm <joakim.uddholm@klarna.com>
2024-04-08 18:31:05 +0200

    feat: show popup after importing a threat model to remind users to review the action items marked in a previous threat model.

---
+++ app/src/components/model/modals/RevisitActionItems.js
@@ -0,0 +1,53 @@
+import VisibilityRoundedIcon from "@mui/icons-material/VisibilityRounded";
+import {
+  Box,
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Paper,
+  Typography,
+} from "@mui/material";
+import React, { useEffect } from "react";
+import { useCloseModal } from "../../../hooks/useCloseModal";
+import { ActionItemList } from "../panels/left/ActionItemList";
+import { useSetShouldReviewActionItemsMutation } from "../../../api/gram/model";
+import { useModelID } from "../hooks/useModelID";
+
+export function RevisitActionItems() {
+  const closeModal = useCloseModal();
+  const modelId = useModelID();
+  const [setShouldReview, _] = useSetShouldReviewActionItemsMutation();
+
+  useEffect(() => {
+    modelId && setShouldReview({ id: modelId, shouldReviewActionItems: false });
+  }, [modelId, setShouldReview]);
+
+  return (
+    <Dialog open={true} scroll={"paper"} fullWidth maxWidth="md">
+      <DialogTitle>
+        <Box display="flex" alignItems="center" gap="10px">
+          <VisibilityRoundedIcon fontSize="large" />
+          <Typography variant="h5">Revisit the action items</Typography>
+        </Box>
+      </DialogTitle>
+      <DialogContent sx={{ paddingTop: "0" }}>
+        <Typography>
+          These threats were marked as action items on from your previous threat
+          model. Please review them and mark them as resolved if they are no
+          longer relevant.
+        </Typography>
+        <br />
+        <Paper sx={{ padding: "15px" }}>
+          <ActionItemList automaticallyExpanded={true} />
+        </Paper>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={() => closeModal()} variant="outlined">
+          Close
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
---
+++ core/src/data/migrations/27_models_add_should_review_action_items.sql
@@ -0,0 +1,2 @@
+ALTER TABLE models
+    ADD should_review_action_items boolean DEFAULT false;
