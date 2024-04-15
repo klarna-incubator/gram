94d0e956ebe2a9428acfbdbd09ffa62e869f8b96
Joakim Uddholm <joakim.uddholm@klarna.com>
2024-04-15 13:30:04 +0200

    SECDEV-1851 upgrade production instance to 16.1 as well, should apply during next maintenance window

--- infra/production/provider.tf
+++ infra/production/provider.tf
@@ -1,5 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region              = "eu-west-1"
   profile             = "iam-sync/gram/gram.IdP_admin@715798949107"
   allowed_account_ids = ["715798949107"]
+
+  default_tags {
+    tags = {
+      SystemID = "gram"
+      Team     = "Secure Development"
+      OhPoliceNamespace = "gram"
+    }
+  }
 }
