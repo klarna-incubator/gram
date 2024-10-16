d212ae181d192e75307ab242a339f28e47757011
Pauline Didier <pauline.didier@klarna.com>
2024-10-16 13:49:12 +0200

    refactor: :art: improve existing rule and endpoint

--- core/src/validation/ValidationHandler.ts
+++ core/src/validation/ValidationHandler.ts
@@ -1,4 +1,7 @@
+import Control from "../data/controls/Control.js";
+import Mitigation from "../data/mitigations/Mitigation.js";
 import Model, { Component, DataFlow } from "../data/models/Model.js";
+import Threat from "../data/threats/Threat.js";
 
 export interface ValidationResult {
   type: "component" | "resource" | "model";
@@ -14,12 +17,23 @@ export interface ValidationProvider {
   validate(model: Model): Promise<ValidationResult[]>;
 }
 
+export interface ModelTestRuleArgs {
+  model: Model;
+}
+export interface ComponentTestRuleArgs {
+  component: Component;
+  dataflows?: DataFlow[];
+  threats?: Threat[];
+  controls?: Control[];
+  mitigations?: Mitigation[];
+}
+
 export interface ComponentValidationRule {
   type: "component";
   name: string;
-  affectedType: ("proc" | "ee" | "ds" | "tf")[];
+  affectedType: ("proc" | "ee" | "ds" | "tb")[];
   conditionalRules?: [string, boolean][]; // ["name of the rule", "result of the test"]
-  test: (component: Component, dataflows: DataFlow[]) => boolean; // Return true if the model follows the rules
+  test: (args: ComponentTestRuleArgs) => Promise<boolean>; // Return true if the model follows the rules
   messageTrue: string;
   messageFalse: string;
 }
@@ -29,7 +43,7 @@ export interface ModelValidationRule {
   name: string;
   affectedType: [];
   conditionalRules?: [string, boolean][]; // ["name of the rule", "result of the test"]
-  test: (model: Model) => boolean; // Return true if the model follows the rules
+  test: (args: ModelTestRuleArgs) => Promise<boolean>; // Return true if the model follows the rules
   messageTrue: string;
   messageFalse: string;
 }
