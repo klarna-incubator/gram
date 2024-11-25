fd462d016bd3b7bafd97ba7a2321248e828a4524
Joakim Uddholm <joakim.uddholm@klarna.com>
2024-11-25 16:41:11 +0100

    chore: remove unused import

--- app/src/components/search/SearchPage.js
+++ app/src/components/search/SearchPage.js
@@ -1,10 +1,10 @@
-import { Card, CardContent, Divider, Typography } from "@mui/material";
+import { Card, CardContent, Typography } from "@mui/material";
+import Grid from "@mui/material/Grid2";
 import React from "react";
 import { useLocation } from "react-router-dom";
-import Loading from "../loading";
-import Grid from "@mui/material/Grid2";
 import { useGetSearchTypesQuery } from "../../api/gram/search";
 import { CenteredPage } from "../elements/CenteredPage";
+import Loading from "../loading";
 import { SearchResultBox } from "./SearchResultBox";
 
 export default function SearchPage() {
