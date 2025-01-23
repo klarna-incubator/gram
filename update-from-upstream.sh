5187c7b22976668ba1179086f6881d048f565458
Joakim Uddholm <joakim.uddholm@klarna.com>
2025-01-23 17:27:59 +0100

    SECDEV-2162 add script

---
+++ update-from-upstream.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+git fetch
+git fetch github # assuming here that github origin is the OSS repo
+
+git checkout develop
+
+# create branch from upstream tag + date
+tag=$(git describe --tags --abbrev=0 github/main)
+branch="github-$tag-$(date +%Y%m%d)"
+git checkout -b $branch $tag
+
+# Pull in changes from upstream
+git pull github main
+
+# Push branch to stash
+git push
