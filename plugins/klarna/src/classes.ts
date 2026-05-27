8125328ca8178f79e9fdf7c44e9c8587d0b2249e
Aleksei Meshcheriakov <alexey.meshcheriakov@klarna.com>
2026-05-27 14:16:39 +0200

    Q3030559: Adding new components to Klarna Tech Stack

--- plugins/klarna/src/classes.ts
+++ plugins/klarna/src/classes.ts
@@ -23,6 +23,102 @@ const classes = [
     icon: "/assets/klarna/chaossearch.png",
     componentType: "any",
   },
+  {
+    id: "a11dd8e8-2789-4c5e-b687-d1163d06adab",
+    name: "LLM",
+    icon: "/assets/klarna/llm.png",
+    componentType: "any",
+  },
+  {
+    id: "382ed605-c6e6-44ea-a4ee-2e6fc2f0e50b",
+    name: "ChatGPT",
+    icon: "/assets/klarna/gpt.png",
+    componentType: "any",
+  },
+  {
+    id: "cfdf0099-c4cd-426e-badb-085930e98fad",
+    name: "Anthropic",
+    icon: "/assets/klarna/anthropic.png",
+    componentType: "any",
+  },
+  {
+    id: "973fe564-df80-43f1-82c7-1175bdd30928",
+    name: "Claude",
+    icon: "/assets/klarna/claude.png",
+    componentType: "any",
+  },
+  {
+    id: "c0982c37-bc9e-4f63-b8a6-8154f38dde73",
+    name: "Gemini",
+    icon: "/assets/klarna/gemini.png",
+    componentType: "any",
+  },
+  {
+    id: "579c7485-262c-4f55-9074-86fb1fa2fc90",
+    name: "Llama",
+    icon: "/assets/klarna/llama.png",
+    componentType: "any",
+  },
+  {
+    id: "06556857-171a-46e8-aacb-5e6820828950",
+    name: "Grok",
+    icon: "/assets/klarna/grok.png",
+    componentType: "any",
+  },
+  {
+    id: "34966f4f-74fc-4efe-8190-cc0434fc79fb",
+    name: "KNS",
+    icon: "/assets/klarna/kns.png",
+    componentType: "any",
+  },
+  {
+    id: "3ee5e976-fe69-4bbc-a609-907b363761e7",
+    name: "KSS",
+    icon: "/assets/klarna/kss.png",
+    componentType: "any",
+  },
+  {
+    id: "7afc3770-39da-493c-bad5-8c3285b16c70",
+    name: "Experimentation Platform",
+    icon: "/assets/klarna/experimentation-platform.png",
+    componentType: "any",
+  },
+  {
+    id: "2fb62233-2492-45ab-b0ed-be90ce388f19",
+    name: "AI Gateway",
+    icon: "/assets/klarna/ai-gateway.png",
+    componentType: "any",
+  },
+  {
+    id: "e0e04b0c-8617-428f-8ec7-206f62480f2b",
+    name: "Scoring Platform",
+    icon: "/assets/klarna/scoring-platform.png",
+    componentType: "any",
+  },
+  {
+    id: "05e78e6b-077c-4870-9088-507afdcef633",
+    name: "KEP Pipelines",
+    icon: "/assets/klarna/pipeline.png",
+    componentType: "any",
+  },
+  {
+    id: "d89db6be-de59-4bfe-9152-7801348d29f2",
+    name: "C2C Managed Kubernetes",
+    icon: "/assets/klarna/mk8s.png",
+    componentType: "any",
+  },
+  {
+    id: "45d954db-ad7a-44a5-932d-f84721854c58",
+    name: "C2C Jobs",
+    icon: "/assets/klarna/job.png",
+    componentType: "any",
+  },
+  {
+    id: "8756834e-7382-43d6-a4cc-e09b74e352ec",
+    name: "",
+    icon: "/assets/klarna/.png",
+    componentType: "any",
+  }
 ];
 
 export default classes;
